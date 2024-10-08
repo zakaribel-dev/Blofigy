f09ce4ddba37ff98a7274695cd25eef51a019224
zakaribel-devzakaribel@hotmail.com <zakaribel@hotmail.com>
2024-10-08 16:05:31 +0200

    doc swagger

--- swagger.js
+++ swagger.js
@@ -1,5 +1,7 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+// la doc : https://swagger-autogen.github.io/docs/getting-started/quick-start/
+
 const outputFile = './swagger-output.yaml';
 const endpointsFiles = ['./routes/userRoutes.js', './routes/postRoutes.js'];
 
